0cc3df6d51f1842639c48cbbd6f1eb421d2ca156
unknown <patrickschuermann1997@gmail.com>
2023-03-10 16:17:16 +0100

    performance optimatizations using numba

--- mode7.py
+++ mode7.py
@@ -1,6 +1,9 @@
 import pygame
 import numpy
 
+# JIT compiler and range function for performance speedup
+from numba import njit, prange
+
 from settings import *
 
 class Mode7:
@@ -22,12 +25,16 @@ class Mode7:
         self.screen_array = pygame.surfarray.array3d(pygame.Surface(WIN_RES))
 
     def update(self):
-        self.screen_array = self.render_frame()
+        self.screen_array = self.render_frame(self.floor_array, self.screen_array, self.tex_size)
 
     # Computes a single frame of the floor texture pixel by pixel.
-    def render_frame(self):
+    # Needs numba support (decorators) to achieve a reasonable framerate when executed every frame.
+    @staticmethod
+    @njit(fastmath=True, parallel=True)
+    def render_frame(floor_array, screen_array, tex_size):
         # Compute color value for every single pixel (i, j).
-        for i in range(WIDTH):
+        # prange function (instead of range function) used for outer loop for performance reasons.
+        for i in prange(WIDTH):
             for j in range(HALF_HEIGHT, HEIGHT):
                 # Let us imagine that the floor texture is tiled infinitely 
                 # in both horizontal and vertical direction on a 2D plane.
@@ -56,15 +63,15 @@ class Mode7:
                 py = y / z * SCALE
 
                 # Compute which pixel of the floor texture is over the 
-                floor_pos = int(px % self.tex_size[0]), int(py % self.tex_size[1])
+                floor_pos = int(px % tex_size[0]), int(py % tex_size[1])
 
                 # look up the respective color in the floor array
-                floor_col = self.floor_array[floor_pos]
+                floor_col = floor_array[floor_pos]
 
                 # fill the computed pixel into the screen array
-                self.screen_array[i, j] = floor_col
+                screen_array[i, j] = floor_col
 
-        return self.screen_array
+        return screen_array
 
     def draw(self):
         # Draws the screen contents that were computed in the render_frame method.
